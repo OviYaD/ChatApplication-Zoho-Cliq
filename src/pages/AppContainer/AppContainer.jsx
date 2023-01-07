b008d252964a47b2aa6af227998c8713261f11b0
OviYaD <oviya.d@codingmart.com>
2023-01-07 09:00:55 +0530

    sidebar added

---
+++ src/pages/AppContainer/AppContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MenuBar from "../../components/MenuBar/MenuBar";
+import MainContainer from '../../components/MainContainer.jsx/MainContainer';
+
+export default function AppContainer(){
+    return <>
+        <MenuBar></MenuBar>
+        <MainContainer></MainContainer>
+    </>;
+}
