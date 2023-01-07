5e8017dea77c0b7c8b9c6cb22cb84d2162d8aeea
OviYaD <oviya.d@codingmart.com>
2023-01-07 08:57:11 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { SignUp } from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+import AppContainer from "./pages/AppContainer/AppContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <SignUp></SignUp> */}
+      {/* <SignIn></SignIn> */}
+      {/* <MenuBar></MenuBar> */}
+      <AppContainer></AppContainer>
     </div>
   );
 }
