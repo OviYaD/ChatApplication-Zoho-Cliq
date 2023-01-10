06838eaec468a7a8bce62baae62a47011267b705
sriraam-raj <112157045+sriraam-raj@users.noreply.github.com>
2023-01-11 00:27:17 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  END_POINT: process.env.REACT_APP_END_POINT ?? "",
+//   END_POINT: process.env.REACT_APP_END_POINT ?? "",
+     END_POINT: "http://54.167.127.5/api",
 };
 
 export default config;
