e1f87b47ba723c50d91b36f114a9de3785a50bfe
OviYaD <oviya.d@codingmart.com>
2023-02-08 00:52:25 +0530

    chat window integrated

--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ChannelDescription from '../ChannelDescription/ChannelDescription';
+import ChannelDescription from '../ChannelComponents/ChannelDescription';
 import InputBox from '../InputBox/InputBox';
 import './ChatWindow.scss';
 import Header from './Header';
@@ -7,10 +7,13 @@ import ChatBody from './ChatBody';
 import MediaFiles from './MediaFiles';
 import { useEffect } from 'react';
 import { getChannelInfo } from '../../api/Channel/Channel';
+import LandingPage from '../LandingPage/LandingPage';
 
-export default function ChatWindow({ chatInfo }) {
+export default function ChatWindow({ newMsg, setWindow, messages, chatInfo, socket, setMessages, setActId }) {
     const [openDes, setOpenStatus] = useState(false);
     const [bodyContent, setBodyContent] = useState("chat");
+
+
 
     // useEffect(() => {
     //     console.log(windowInfo)
@@ -24,11 +27,11 @@ export default function ChatWindow({ chatInfo }) {
     // }, [windowInfo])
     return <>
         <div className="" style={{ backgroundColor: "#fff", width: "100%", borderRadius: "10px", marginLeft: "2px", marginRight: "8px", position: "relative" }}>
-            <Header chatInfo={chatInfo} bodyContent={bodyContent} setBodyContent={setBodyContent} setOpenStatus={setOpenStatus} ></Header>
+            <Header setActId={setActId} setWindow={setWindow} chatInfo={chatInfo} bodyContent={bodyContent} setBodyContent={setBodyContent} setOpenStatus={setOpenStatus} ></Header>
 
             {bodyContent === "chat" ? <>
-                <ChatBody></ChatBody>
-                <InputBox></InputBox>
+                <ChatBody newMsg={newMsg} socket={socket} messages={messages} chatInfo={chatInfo}></ChatBody>
+                <InputBox messages={messages} setMessages={setMessages} socket={socket}></InputBox>
             </> : <MediaFiles></MediaFiles>}
         </div>
         {openDes && <ChannelDescription chatInfo={chatInfo} setOpenStatus={setOpenStatus} ></ChannelDescription>}
