d2b24c0798d21d3f51acadbc992d77d09283c656
OviYaD <oviya.d@codingmart.com>
2023-02-08 01:08:19 +0530

    code clean

--- src/components/Validators/mobileNumber.js
+++ src/components/Validators/mobileNumber.js
@@ -2,10 +2,10 @@ import validator from "validator";
 
 export const validateMobileNumber = (number) => {
   const isValidPhoneNumber = validator.isMobilePhone(number);
-  console.log(
-    number,
-    number.length < 10 || number.length > 10 || isValidPhoneNumber
-  );
+  // console.log(
+  //   number,
+  //   number.length < 10 || number.length > 10 || isValidPhoneNumber
+  // );
   return true;
   // return number.length < 10 || number.length > 10 || isValidPhoneNumber;
 };
