c2ba6975211511565733ab6a11acb4c3b46ab09d
OviYaD <oviya.d@codingmart.com>
2023-02-17 15:19:27 +0530

    unread count bug fixed

--- src/components/ChatList/chatList.jsx
+++ src/components/ChatList/chatList.jsx
@@ -134,7 +134,7 @@ export default function ChatList({ unreadCount, setUnreadCount, actId, socket, s
                                                 <div className='options dflx'>
                                                     {unreadCount[chats.chat_id] && <span className='notify' style={{ fontFamily: "zoho-puvi-semi-bold", color: "#fff", backgroundColor: "var(--color-red)", borderRadius: "100%" }}>{unreadCount[chats.chat_id]}</span>}
 
-                                                    <div title="Unpin" onClick={() => upinChats(socket, chats.chat_id)}>
+                                                    <div title="Unpin" className='hideShow' onClick={() => upinChats(socket, chats.chat_id)}>
 
                                                         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" fill="currentColor" width="10" height="10"  >
                                                             <g>
@@ -172,13 +172,15 @@ export default function ChatList({ unreadCount, setUnreadCount, actId, socket, s
                                         <span className='pL1 ellips'>{chats.channel ? chats.channel.name : chats.user.first_name + " " + chats.user.last_name}</span>
                                         <div className='options dflx'>
                                             {unreadCount[chats.chat_id] && <span className='notify' style={{ fontFamily: "zoho-puvi-semi-bold", color: "#fff", backgroundColor: "var(--color-red)", borderRadius: "100%" }}>{unreadCount[chats.chat_id]}</span>}
+                                            <span className='hideShow flexC'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-pin-angle-fill" viewBox="0 0 16 16" onClick={() => pinChats(socket, chats.chat_id, chats.chat_type)}>
+                                                    <path d="M9.828.722a.5.5 0 0 1 .354.146l4.95 4.95a.5.5 0 0 1 0 .707c-.48.48-1.072.588-1.503.588-.177 0-.335-.018-.46-.039l-3.134 3.134a5.927 5.927 0 0 1 .16 1.013c.046.702-.032 1.687-.72 2.375a.5.5 0 0 1-.707 0l-2.829-2.828-3.182 3.182c-.195.195-1.219.902-1.414.707-.195-.195.512-1.22.707-1.414l3.182-3.182-2.828-2.829a.5.5 0 0 1 0-.707c.688-.688 1.673-.767 2.375-.72a5.922 5.922 0 0 1 1.013.16l3.134-3.133a2.772 2.772 0 0 1-.04-.461c0-.43.108-1.022.589-1.503a.5.5 0 0 1 .353-.146z" />
+                                                </svg>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                                                </svg>
 
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-pin-angle-fill" viewBox="0 0 16 16" onClick={() => pinChats(socket, chats.chat_id, chats.chat_type)}>
-                                                <path d="M9.828.722a.5.5 0 0 1 .354.146l4.95 4.95a.5.5 0 0 1 0 .707c-.48.48-1.072.588-1.503.588-.177 0-.335-.018-.46-.039l-3.134 3.134a5.927 5.927 0 0 1 .16 1.013c.046.702-.032 1.687-.72 2.375a.5.5 0 0 1-.707 0l-2.829-2.828-3.182 3.182c-.195.195-1.219.902-1.414.707-.195-.195.512-1.22.707-1.414l3.182-3.182-2.828-2.829a.5.5 0 0 1 0-.707c.688-.688 1.673-.767 2.375-.72a5.922 5.922 0 0 1 1.013.16l3.134-3.133a2.772 2.772 0 0 1-.04-.461c0-.43.108-1.022.589-1.503a.5.5 0 0 1 .353-.146z" />
-                                            </svg>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
-                                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-                                            </svg>
+                                            </span>
                                         </div>
                                         {showProfile === chats.chat_id && <ProfileInfoPopUp chatInfo={chats}></ProfileInfoPopUp>}
                                     </div>
